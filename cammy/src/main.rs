bef70f80fbfcf64bd6c6f01395cf5ac363e972c7
Magnus Ulimoen <flymagnus@gmail.com>
2018-08-01 15:45:15 +0200

    add mirror program

--- cammy/src/main.rs
+++ cammy/src/main.rs
@@ -1,15 +1,18 @@
+#[macro_use]
+extern crate glium;
 extern crate png;
 extern crate uvc_rs;
 
-use uvc_rs::*;
-
-use png::HasParameters;
 use std::fs::File;
 use std::io::BufWriter;
-
 use std::sync::{Arc, Mutex};
 
-fn frame_to_png(frame: &Frame, count: &mut Arc<Mutex<u32>>) {
+use glium::Surface;
+use png::HasParameters;
+use uvc_rs::*;
+
+#[allow(unused)]
+fn callback_frame_to_png(frame: &Frame, count: &mut Arc<Mutex<u32>>) {
     let new_frame = frame.to_rgb().unwrap();
 
     let bytes = new_frame.to_bytes();
@@ -31,6 +34,15 @@ fn frame_to_png(frame: &Frame, count: &mut Arc<Mutex<u32>>) {
     writer.write_image_data(&bytes).unwrap();
 }
 
+#[allow(unused)]
+fn callback_frame_to_vec(frame: &Frame, count: &mut Arc<Mutex<Option<Frame>>>) {
+    let new_frame = frame.to_rgb().unwrap();
+
+    let mut data = Mutex::lock(&count).unwrap();
+
+    *data = Some(new_frame);
+}
+
 fn main() {
     let ctx = Context::new().expect("Could not create context");
     let mut list = ctx.devices().expect("Could not get devices");
@@ -48,13 +60,124 @@ fn main() {
 
     println!("{:#?}", ctrl);
 
-    let calls = Arc::new(Mutex::new(0u32));
-    let stream = ctrl
-        .start_streaming(&devh, frame_to_png, calls.clone())
+    let calls = Arc::new(Mutex::new(None));
+    let _stream = ctrl
+        .start_streaming(&devh, callback_frame_to_vec, calls.clone())
         .unwrap();
 
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    use glium::glutin;
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new();
+    let context = glium::glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    stream.stop();
-    println!("Number of captured frames: {}", calls.lock().unwrap());
+    #[derive(Copy, Clone)]
+    pub struct QuadVertex {
+        pos: (f32, f32),
+    }
+
+    implement_vertex!(QuadVertex, pos);
+
+    let vertices: [QuadVertex; 4] = [
+        QuadVertex { pos: (-1.0, -1.0) },
+        QuadVertex { pos: (-1.0, 1.0) },
+        QuadVertex { pos: (1.0, -1.0) },
+        QuadVertex { pos: (1.0, 1.0) },
+    ];
+
+    let indices: [u8; 6] = [0, 1, 2, 1, 3, 2];
+
+    let vertex_shader_source = r#"
+    #version 140
+
+    in vec2 pos;
+
+    out vec2 v_position;
+
+    void main() {
+        v_position = (pos + 1.0)/2.0;
+        gl_Position = vec4(-pos.x, -pos.y, 0.0, 1.0);
+    }
+    "#;
+
+    let fragment_shader_source = r#"
+    #version 140
+
+    in vec2 v_position;
+
+    out vec4 colour;
+
+    uniform sampler2D u_image;
+
+    void main() {
+        vec2 pos = v_position;
+
+        colour = texture(u_image, pos);
+    }
+    "#;
+
+    let vertices = glium::VertexBuffer::new(&display, &vertices).unwrap();
+    let indices = glium::IndexBuffer::new(
+        &display,
+        glium::index::PrimitiveType::TrianglesList,
+        &indices,
+    ).unwrap();
+    let program =
+        glium::Program::from_source(&display, vertex_shader_source, fragment_shader_source, None)
+            .unwrap();
+
+    let mut closed = false;
+    let mut buffer = None;
+    while !closed {
+        events_loop.poll_events(|ev| match ev {
+            glutin::Event::WindowEvent { event, .. } => match event {
+                glutin::WindowEvent::CloseRequested => closed = true,
+                _ => {}
+            },
+            _ => {}
+        });
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        let mut mutex = Mutex::lock(&calls).unwrap();
+
+        let frame: Option<Frame> = mutex.take();
+        match frame {
+            None => {
+                // No new frame to render
+            }
+            Some(frame) => {
+                let data_bytes = frame.to_bytes();
+
+                let image = glium::texture::RawImage2d::from_raw_rgb(
+                    data_bytes.to_vec(),
+                    (frame.width(), frame.height()),
+                );
+
+                buffer = Some(
+                    glium::texture::SrgbTexture2d::with_format(
+                        &display,
+                        image,
+                        glium::texture::SrgbFormat::U8U8U8,
+                        glium::texture::MipmapsOption::NoMipmap,
+                    ).unwrap(),
+                );
+            }
+        }
+
+        if let Some(ref b) = buffer {
+            let uniforms = uniform! { u_image: b };
+            target
+                .draw(
+                    &vertices,
+                    &indices,
+                    &program,
+                    &uniforms,
+                    &Default::default(),
+                ).unwrap();
+        }
+
+        target.finish().unwrap();
+    }
 }
