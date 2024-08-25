f3b8bdcb4dac9defcacee130caef378f334d0e70
LucaCiucci <luca.ciucci99@gmail.com>
2024-08-26 00:02:52 +0200

    chore: Update build.rs to include target triple for clang_args

--- uvc-sys/build.rs
+++ uvc-sys/build.rs
@@ -4,6 +4,8 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let target_triple = env::var("TARGET").unwrap();
+
     let mut includedir = None;
     if std::env::var_os("CARGO_FEATURE_VENDOR").is_some() {
         includedir = Some(std::env::var("DEP_UVCSRC_INCLUDE").unwrap());
@@ -24,6 +26,9 @@ fn main() {
         .header("wrapper.h")
         .allowlist_function("uvc_.*")
         .allowlist_type("uvc_.*")
+        .clang_args([
+            "-target", &target_triple,
+        ])
         .generate()
         .expect("Failed to generate bindings");
 
